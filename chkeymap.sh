decceb971a3f104657942a4ba2b98881264222ab
nuvhandra <k3ll3x@gmail.com>
2021-02-16 18:08:54 -0600

    chkeymap script added, change keymap from us to latam an viseversa, this script can be added to WM keybindings ;)

--- chkeymap.sh
+++ chkeymap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$(setxkbmap -query | grep 'layout' | awk '/\w+/ {print $2}')" == "us" ];
 then
-	nofify-send 'Setting latam'
+	notify-send 'Setting latam'
 	setxkbmap latam
 else
 	notify-send 'Setting us'
