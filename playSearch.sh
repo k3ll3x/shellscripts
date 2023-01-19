ebf1f1b32fa222983931e047dc7cf55fbbbfc5bc
k3ll3x <k3ll3x@gmail.com>
2023-01-19 12:13:07 +0100

    playSearch, xargs fixed

--- playSearch.sh
+++ playSearch.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 srch=$1
-echo $srch
+echo "Search: $srch"
 if [[ -z "$srch" ]]
 then
 	echo
 else
 #	find ~/Music -iname "*$srch*" -print0 | xargs -0 -I{} mplayer {}
-	find ~/Music -iname "*$srch*" -printf "\"%p\" " | xargs sh -c 'mplayer "$@" < /dev/tty'
+#	find ~/Music -iname "*$srch*" -printf "\"%p\" " | xargs sh -c 'echo "$@"; mplayer "$@" < /dev/tty'
+	find ~/Music -iname "*$srch*" -print0 | xargs -0 -o mplayer
+	sleep 1
 fi
